d83f1002335b9fc28e05659326964615821567ba
Tatiana Magdalena <tmagdalena@stone.com.br>
2017-11-28 15:39:55 -0200

    Add localization

---
+++ objc/Demo Objc/NSString+Utils.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Utils.h
+//  Demo Objc
+//
+//  Created by Tatiana Magdalena on 27/11/17.
+//  Copyright © 2017 Eduardo Mello de Macedo | Stone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString_Utils : NSObject
+
+@end
---
+++ objc/Demo Objc/NSString+Utils.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Utils.m
+//  Demo Objc
+//
+//  Created by Tatiana Magdalena on 27/11/17.
+//  Copyright © 2017 Eduardo Mello de Macedo | Stone. All rights reserved.
+//
+
+#import "NSString+Utils.h"
+
+@implementation NSString_Utils
+
+@end
