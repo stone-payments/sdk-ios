9424715f77ed9158d37d28726caa79a1d3857837
Tatiana Magdalena <tmagdalena@stone.com.br>
2017-12-15 18:50:48 -0200

    Update ObjcDemo SDK framework to version 2.3.0

--- objc/StoneSDK.framework/Headers/STNPinpad.h
+++ objc/StoneSDK.framework/Headers/STNPinpad.h
@@ -10,12 +10,17 @@
 
 @interface STNPinpad : NSObject
 
+/// Device name.
 @property (nonatomic, readonly, strong) NSString *name;
+/// the UUID for BLE devices or connection ID for classic devices.
 @property (nonatomic, readonly, strong) NSString *identifier;
+/// Custom name.
 @property (nonatomic, strong) NSString *alias;
 
+/// A CBPeripheral if device is Bluetooth Low Energy, or an EAAccessory otherwise.
 @property (nonatomic, readonly, retain) id device;
 
+/// Initialize with a CBPeripheral or EAAccessory object.
 -(instancetype)initWithDevice:(id)device;
 
 @end
