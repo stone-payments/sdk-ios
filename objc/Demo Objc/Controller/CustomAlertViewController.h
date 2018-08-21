b88ac1b5b08126ee432c9db307681dbca8faf260
Bruno Colombini | Stone <bruno.colombini@BRRIOMD011736.local>
2018-08-21 20:46:15 -0300

    Create customalertviewcontroller and add action sheet to change stone code

---
+++ objc/Demo Objc/Controller/CustomAlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomAlertController.h
+//  Demo Objc
+//
+//  Created by Bruno Colombini | Stone on 21/08/18.
+//  Copyright © 2018 Eduardo Mello de Macedo | Stone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface CustomAlertViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
+
+@end
