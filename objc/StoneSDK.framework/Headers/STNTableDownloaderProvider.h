9b31d4045eb66c811dad540e2a68d1ff5cde7870
Bruno Colombini | Stone <bruno.colombini@BRRIOMD011736.local>
2018-09-04 20:14:40 -0300

    add old StoneFramework.framework

---
+++ objc/StoneSDK.framework/Headers/STNTableDownloaderProvider.h
@@ -0,0 +1,24 @@
+//
+//  DownloadTablesProvider.h
+//  StoneSDK
+//
+//  Created by Stone  on 10/14/15.
+//  Copyright © 2015 Stone . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "STNMerchantModel.h"
+
+@interface STNTableDownloaderProvider : NSObject
+
+/// Downloads AID and CAPK tables from server. Uses the first activated merchant.
++ (void)downLoadTables:(void (^)(BOOL succeeded, NSError *error))block __deprecated_msg("no need anymore.");
+
+/**
+ Downloads AID and CAPK tables from server from a merchant.
+
+ @param merchant The merchant from which the tables will be downloaded.
+ */
++ (void)downLoadTablesFromMerchant:(STNMerchantModel*)merchant withBlock:(void (^)(BOOL succeeded, NSError *error))block __deprecated_msg("no need anymore.");
+
+@end
